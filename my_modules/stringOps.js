122d15350182415fa6471b85d513298305e2a94c
miguelbriceno <mabo544@gmail.com>
2022-07-01 16:27:27 -0500

    Add function toUpperFirst

--- my_modules/stringOps.js
+++ my_modules/stringOps.js
@@ -27,3 +27,39 @@ module.exports.countChar = (cadena, c) => {
   dataBack.push(posArray);
   return dataBack;
 }
+
+/*------------------------------------------------------------------------------
+-----------------------------FRIST CHAR TO UPPERCASE----------------------------
+------------------------------------------------------------------------------*/
+// Receives a string and converts the first letter of each word to uppercase and the rest to lowercase.
+module.exports.toUpperFirst = (cadena) => {
+  let newCadena = [];
+  let isBlankSpace = /\s/;
+  let isValid = /\w/;
+  let firstValidCharFlag = false;
+  // 0) Validate that the received data is a string.
+  if (typeof cadena != 'string'){
+    return console.log("Erro0: Por favor ingresa una cadena válida.");
+  }
+  // 1) Convert the entire string to lowercase and then convert it to an array so it can be traversed.
+  cadena = cadena.toLowerCase();
+  cadena = Array.from(cadena);
+  // 2) Go through the string lookin for the chars to modify
+  cadena.forEach((char, i)=>{
+    if(isValid.test(char) === true && firstValidCharFlag === false){ // Is it a valid char?
+      firstValidCharFlag = true;
+      newCadena.push(char.toUpperCase());
+    } else if (isValid.test(char) === true && firstValidCharFlag === true){
+      if(isBlankSpace.test(cadena[i-1]) === true){ // Is it preceded by a blank space?
+        newCadena.push(char.toUpperCase());
+      } else {
+        newCadena.push(char);
+      }
+    } else {
+      newCadena.push(char);
+    }
+  });
+  // 3) Convert the new array to a string and return it.
+  newCadena = newCadena.join("");
+  return newCadena;
+}
